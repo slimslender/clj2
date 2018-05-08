cce8076bdaa24d0f127a08bd9cb7aa350500af47
Jim Clark <slim@Jims-MacBook-Pro.local>
2018-05-08 16:30:11 -0700

    hmmm

--- project.clj
+++ project.clj
@@ -1,5 +1,5 @@
  (defproject clj1 "0.1.0-SNAPSHOT"
-   :description "FIXME: write description"
+   :description "XME: write description"
    :dependencies [[org.clojure/clojure "1.8.0"]
                   [metosin/compojure-api "1.1.11"]]
    :ring {:handler clj1.handler/app}
