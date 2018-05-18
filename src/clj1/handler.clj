caf0804eab01b5b05b74f11aa9780b908e66d4fe
Jim Clark <slimslenderslacks@gmail.com>
2018-05-17 17:13:05 -0700

    Autofix: cljformat
    [atomist:generated]

--- src/clj1/handler.clj
+++ src/clj1/handler.clj
@@ -15,7 +15,6 @@
    :with :mushrooms
    :and :olives})
 
-
 (def app
   (api
    {:swagger
@@ -23,8 +22,7 @@
      :spec "/swagger.json"
      :data {:info {:title "Clj1"
                    :description "Compojure Api example"}
-               :tags [{:name "api", :description "some apis"}]}}}
-  
+            :tags [{:name "api", :description "some apis"}]}}}
 
    (context "/api" []
      :tags ["api"]
